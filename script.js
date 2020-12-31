4ef93d3f82f904d827b727fd7f0258acee84739f
qmya <qandeelgul90@gmail.com>
2020-12-31 14:35:04 -0500

    created a link for google map

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//Using Geo-location API : navigator.geolocation.getCurrentPosition()
+//Here it will take two function one when the position is successfully getted and the other is when it is failed
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    console.log(position);
+
+    // const latitude = position.coords.latitude;
+    const { latitude } = position.coords;
+    // const longitude = position.coords.longitude;
+    const { longitude } = position.coords;
+    console.log(latitude, longitude);
+    console.log(`https://www.google.ca/maps/@${latitude},${longitude}`);
+  },
+  function () {
+    alert('Could not get your position');
+  }
+);
