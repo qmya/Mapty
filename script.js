7ff1a31488d6f02c9b9adc85e3b4143588df8665
qmya <qandeelgul90@gmail.com>
2021-01-01 18:26:38 -0500

    added the Parent class with two children class for run and cycling feature

--- script.js
+++ script.js
@@ -11,6 +11,48 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10); //We shouldnt create any id by our own we should use a library to take care of it
+  constructor(coords, distance, duration) {
+    this.coords = coords; //[lat, lng]
+    this.distance = distance; //in km
+    this.duration = duration; //in mins
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  //Calulating the Pace
+  calcPace() {
+    //Pace is define as min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    //km/hr
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([43, -79], 5.2, 24, 178);
+// const cycling1 = new Cycling([43, -79], 27, 95, 523);
+// console.log(run1, cycling1);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//APPLICATION ARCHITECTURE :
 class App {
   //To make it encapsulated and make it part of the class object:
   #map;
